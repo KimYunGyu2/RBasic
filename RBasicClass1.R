adfa63e8e16962bf71ee5eeb35e2561b58ea04dc
KimYunGyu2 <kyg839912@naver.com>
2022-04-18 10:33:41 +0900

    12

---
+++ RBasicClass1.R
@@ -0,0 +1,18 @@
+dim(available.packages())
+
+sessionInfo()
+
+install.packages("stringr")
+
+library("stringr")
+
+search()  # 현재 로드된 패키지 확인인
+
+remove.packages("stringr")
+
+data()
+
+hist(Nile)
+
+# 변수 
+
